be198169d23e6689f9086c39abcca86cf04a6d8c
VictorA1992 <119535368+VictorA1992@users.noreply.github.com>
2023-02-13 03:01:57 +0300

    Add files via upload

---
+++ src/tasks/SubTask.java
@@ -0,0 +1,29 @@
+package tasks;
+
+public class SubTask extends Task {
+
+    private Integer epicId;
+
+    public SubTask(String name, String description, Status status, Integer epicId) {
+        super(name, description, status.NEW);
+        this.epicId = epicId;
+    }
+
+    public Integer getEpicId() {
+        return this.epicId;
+    }
+
+
+    public String toString() {
+
+        return "Subtask{" +
+
+                "epicID=" + epicId +
+
+                ", name='" + getName() + '\'' +
+                ", description='" + getDescription() + '\'' +
+                ", status=" + getStatus() +
+                '}';
+
+    }
+}
