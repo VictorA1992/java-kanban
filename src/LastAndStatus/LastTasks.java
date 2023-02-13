f0311572479f3d962217e9537bbc77aa467acd55
VictorA1992 <119535368+VictorA1992@users.noreply.github.com>
2023-02-13 03:00:38 +0300

    Add files via upload

---
+++ src/LastAndStatus/LastTasks.java
@@ -0,0 +1,22 @@
+package LastAndStatus;
+
+import tasks.Task;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.LinkedList;
+
+public class LastTasks {
+    private static final int TASKS_TO_SAVE = 10;
+    private final LinkedList<Task> lastTasks = new LinkedList<Task>();
+
+    public void addTask(Task task) {
+        if (lastTasks.size() == TASKS_TO_SAVE) lastTasks.removeLast();
+        lastTasks.addFirst(task);
+    }
+
+    public List<Task> getHistory() {
+        return new ArrayList<>(lastTasks);
+    }
+
+}
--- src/LastAndStatus/TaskStatus.java
+++ src/LastAndStatus/TaskStatus.java
@@ -1,1 +1,23 @@
+package LastAndStatus;
 
+public enum TaskStatus {
+    NEW(0), IN_PROGRESS(1), DONE(2);
+    private final int value;
+
+    TaskStatus(int value) {
+        this.value = value;
+    }
+
+    public static TaskStatus getType (int id) {
+        for (TaskStatus type : values()) {
+            if (type.getValue() == id) {
+                return type;
+            }
+        }
+        return null;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
