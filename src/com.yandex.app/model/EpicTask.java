4359584996b8706f06f83a87a5c7d349adfea301
VictorA1992 <119535368+VictorA1992@users.noreply.github.com>
2023-03-01 01:44:31 +0300

    Add files via upload

---
+++ src/com.yandex.app/model/EpicTask.java
@@ -0,0 +1,37 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EpicTask extends Task {
+
+    private final List<Integer> subtasksId = new ArrayList<>();
+
+    public EpicTask(String name, String description) {
+
+        super(name, description, Status.NEW);
+    }
+
+    public List<Integer> getSubtasksId() {
+        return this.subtasksId;
+    }
+
+    public void addSubtaskId(Integer keyId) {
+        this.subtasksId.add(keyId);
+    }
+
+    @Override
+
+    public String toString() {
+
+        return "tasks.EpicTask{" +
+
+                "subtasksID=" + subtasksId +
+
+                ", name='" + getName() + '\'' +
+                ", description='" + getDescription() + '\'' +
+                ", status=" + getStatus() +
+                '}';
+
+    }
+}
