e8a3c5769567723327965e6a34955f94b67339d2
VictorA1992 <119535368+VictorA1992@users.noreply.github.com>
2023-03-01 01:45:31 +0300

    Add files via upload

--- src/com.yandex.app/lastAndStatus/LastTasks.java
+++ src/com.yandex.app/lastAndStatus/LastTasks.java
@@ -1,1 +1,22 @@
+package LastAndStatus;
 
+import model.Task;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.LinkedList;
+
+public class LastTasks {
+    private static final int TASKS_TO_SAVE = 10;
+    private final LinkedList<Task> lastTasks = new LinkedList<>();
+
+    public void addTask(Task task) {
+        if (lastTasks.size() == TASKS_TO_SAVE) lastTasks.removeLast();
+        lastTasks.addFirst(task);
+    }
+
+    public List<Task> getHistory() {
+        return new ArrayList<>(lastTasks);
+    }
+
+}
